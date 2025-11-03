b2e0f25e8ef647f157deaac7230825ec85f9f930
systemforgames <systemforgames@gmail.com>
2025-11-03 20:52:14 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import urllib.request
+import xml.etree.ElementTree as ET
+import requests
+import sys
+
+print("hello world")
+a = 5
+b = 3
+c = 3
+
+print(a + b + c)
+
+print(a)
+
+if a == 5: 
+    print("e 5")
+else:
+    print("nu e 5")
+    print("asd")
+    
